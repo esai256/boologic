a02f62c1f6f04edb02245bfc49ab10ee69ec4b46
Elias Seichter <esai.n900@gmail.com>
2017-09-30 22:04:39 +0200

    Entwicklung vorbereitet

---
+++ dist/index.js
@@ -0,0 +1,48 @@
+define(["exports"], function (exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var _createClass = function () {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ("value" in descriptor) descriptor.writable = true;
+                Object.defineProperty(target, descriptor.key, descriptor);
+            }
+        }
+
+        return function (Constructor, protoProps, staticProps) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps);
+            if (staticProps) defineProperties(Constructor, staticProps);
+            return Constructor;
+        };
+    }();
+
+    var Test = exports.Test = function () {
+        function Test() {
+            _classCallCheck(this, Test);
+        }
+
+        _createClass(Test, [{
+            key: "a",
+            get: function get() {
+                return 5;
+            }
+        }]);
+
+        return Test;
+    }();
+
+    alert("test2 Dinge und Sachen");
+});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export class Test {
+    get a() {
+        return 5;
+    }
+}
+
+alert("test2 Dinge und Sachen");
